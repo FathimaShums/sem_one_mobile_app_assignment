229ba1a79dc241091aef4aaa18f419b36d315552
FathimaShums <fathimashums416@gmail.com>
2024-09-13 15:52:49 +0530

    added a network image

--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -11,15 +11,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<FoodItem> items = [
-    FoodItem(1, "Maqluba", "rice,you turn the pot upside down when serving", "",
-        50.03, "Rice"),
+    FoodItem(
+        1,
+        "Maqluba",
+        "rice,you turn the pot upside down when serving",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcLhJZfKMziE5if_GMu-SiF09pSq8XY3KT_g&s",
+        50.03,
+        "Rice"),
   ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: items.length,
       itemBuilder: (context, index) {
-        FoodItemCard(
+        return FoodItemCard(
           TheFoodItem: items[index],
         );
       },
