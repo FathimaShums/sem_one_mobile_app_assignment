a87ed132c08019d3f35b69ed5499995054dda31e
FathimaShums <fathimashums416@gmail.com>
2024-09-13 15:30:47 +0530

    attempt to load list

--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:sem_one_mobile_app_assignment/models/FoodItem.dart';
+import 'package:sem_one_mobile_app_assignment/shared/FoodItemCard.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<FoodItem> items = [
+    FoodItem(1, "Maqluba", "rice,you turn the pot upside down when serving", "",
+        50.03, "Rice"),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        FoodItemCard(
+          TheFoodItem: items[index],
+        );
+      },
+    );
   }
 }
