63ea5066d2340c9f347f96ab07578644c84025f7
FathimaShums <fathimashums416@gmail.com>
2024-09-13 15:30:33 +0530

    stucture for an item

---
+++ lib/shared/FoodItemCard.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:sem_one_mobile_app_assignment/models/FoodItem.dart';
+
+class FoodItemCard extends StatefulWidget {
+  const FoodItemCard({super.key, required this.TheFoodItem});
+  final FoodItem TheFoodItem;
+
+  @override
+  State<FoodItemCard> createState() => _FoodItemCardState();
+}
+
+class _FoodItemCardState extends State<FoodItemCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image(
+          image: NetworkImage(widget.TheFoodItem.image),
+        ),
+        Column(
+          children: [
+            Text(widget.TheFoodItem.title),
+            Text(widget.TheFoodItem.description),
+            Text(widget.TheFoodItem.price as String),
+          ],
+        )
+      ],
+    );
+  }
+}
