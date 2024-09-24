ec8ec5fc91e64687c0acfb921b3fae72ecbf6af9
FathimaShums <fathimashums416@gmail.com>
2024-09-24 18:58:52 +0530

    items can now be added to favourites

--- lib/shared/FoodItemCard.dart
+++ lib/shared/FoodItemCard.dart
@@ -3,9 +3,13 @@ import 'package:sem_one_mobile_app_assignment/models/FoodItem.dart';
 
 class FoodItemCard extends StatefulWidget {
   const FoodItemCard(
-      {super.key, required this.TheFoodItem, required this.onPressed});
+      {super.key,
+      required this.TheFoodItem,
+      required this.onPressed,
+      required this.whenPressed});
   final FoodItem TheFoodItem;
   final VoidCallback onPressed;
+  final VoidCallback whenPressed;
 
   @override
   State<FoodItemCard> createState() => _FoodItemCardState();
@@ -24,7 +28,13 @@ class _FoodItemCardState extends State<FoodItemCard> {
             Text(widget.TheFoodItem.title),
             Text(widget.TheFoodItem.description),
             ElevatedButton(
-                onPressed: widget.onPressed, child: Text("Add to Cart"))
+              onPressed: widget.onPressed,
+              child: Text("Add to Cart"),
+            ),
+            ElevatedButton(
+              onPressed: widget.whenPressed,
+              child: Text("Favourite"),
+            ),
             // Text(widget.TheFoodItem.price as String),
           ],
         )
