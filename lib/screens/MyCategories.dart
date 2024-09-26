d3a0ed9cdbf462db636cae0d1cdd799f99e5dabc
FathimaShums <fathimashums416@gmail.com>
2024-09-26 08:39:56 +0530

    orientation change functionality works

--- lib/screens/MyCategories.dart
+++ lib/screens/MyCategories.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sem_one_mobile_app_assignment/models/TheCategory.dart';
+import 'package:sem_one_mobile_app_assignment/screens/login.dart';
+import 'package:sem_one_mobile_app_assignment/screens/rice.dart';
 import 'package:sem_one_mobile_app_assignment/shared/Acategory.dart';
 
 class MyCategories extends StatelessWidget {
@@ -8,78 +10,123 @@ class MyCategories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      orientation == Orientation.portrait
-          ? Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ACategory(
-                      CategoryItem: TheCategory("Rice",
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s"),
+    return orientation == Orientation.portrait
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ACategory(
+                    CategoryItem: TheCategory(
+                      "Bread",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
                     ),
-                    ACategory(
-                      CategoryItem: TheCategory("Bread",
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s"),
+                    navigateToPage: rice(),
+                  ),
+                  ACategory(
+                    CategoryItem: TheCategory(
+                      "Bread",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
                     ),
-                    ACategory(
-                      CategoryItem: TheCategory("Salads",
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s"),
+                    navigateToPage: rice(),
+                  ),
+                  ACategory(
+                    CategoryItem: TheCategory(
+                      "Salads",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
                     ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ACategory(
-                      CategoryItem: TheCategory("Rice",
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s"),
+                    navigateToPage: rice(),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ACategory(
+                    CategoryItem: TheCategory(
+                      "Rice",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
                     ),
-                    ACategory(
-                      CategoryItem: TheCategory("Rice",
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s"),
+                    navigateToPage: rice(),
+                  ),
+                  ACategory(
+                    CategoryItem: TheCategory(
+                      "Rice",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
                     ),
-                    ACategory(
-                      CategoryItem: TheCategory("Rice",
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s"),
+                    navigateToPage: rice(),
+                  ),
+                  ACategory(
+                    CategoryItem: TheCategory(
+                      "Rice",
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
                     ),
-                  ],
-                )
-              ],
-            )
-          : Text("landscape mode")
-      //       : Column(
-      //           children: [
-      //             Row(
-      //               children: [
-      //                 ACategory(
-      //                   CategoryItem: TheCategory(
-      //                     "Rice",
-      //                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-      //                   ),
-      //                 ),
-      //                 ACategory(
-      //                   CategoryItem: TheCategory(
-      //                     "Rice",
-      //                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-      //                   ),
-      //                 ),
-      //                 ACategory(
-      //                   CategoryItem: TheCategory(
-      //                     "Rice",
-      //                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-      //                   ),
-      //                 ),
-      //               ],
-
-      //             ),
-      //           ],
-      //         )
-    ]);
-    // Text(orientation == Orientation.portrait
-    //     ? 'my categories in potrait mode'
-    //     : 'my Categories Page');
+                    navigateToPage: rice(),
+                  ),
+                ],
+              ),
+            ],
+          )
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                    child: ACategory(
+                      CategoryItem: TheCategory(
+                        "Rice",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                      ),
+                      navigateToPage: rice(),
+                    ),
+                  ),
+                  Flexible(
+                    child: ACategory(
+                      CategoryItem: TheCategory(
+                        "Bread",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
+                      ),
+                      navigateToPage: rice(),
+                    ),
+                  ),
+                  Flexible(
+                    child: ACategory(
+                      CategoryItem: TheCategory(
+                        "Salads",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                      ),
+                      navigateToPage: rice(),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                    child: ACategory(
+                      CategoryItem: TheCategory(
+                        "Pasta",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                      ),
+                      navigateToPage: LoginPage(),
+                    ),
+                  ),
+                  Flexible(
+                    child: ACategory(
+                      CategoryItem: TheCategory(
+                        "Pizza",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
+                      ),
+                      navigateToPage: rice(),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
   }
 }
