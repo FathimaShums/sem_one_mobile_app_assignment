0ea9599f419f6f852416eecca650cced15052a99
FathimaShums <fathimashums416@gmail.com>
2024-09-27 10:39:02 +0530

    added a sign up page

---
+++ lib/screens/signup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class mysignUp extends StatefulWidget {
+  const mysignUp({super.key});
+
+  @override
+  State<mysignUp> createState() => _mysignUpState();
+}
+
+class _mysignUpState extends State<mysignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
