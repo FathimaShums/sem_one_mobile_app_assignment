5eaf7f94aeacfc62a61c9e981eff0121e3f68a82
FathimaShums <fathimashums416@gmail.com>
2024-09-27 10:38:52 +0530

    removed redundant package

--- lib/screens/MyCategories.dart
+++ lib/screens/MyCategories.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sem_one_mobile_app_assignment/models/TheCategory.dart';
 import 'package:sem_one_mobile_app_assignment/screens/login.dart';
-import 'package:sem_one_mobile_app_assignment/screens/rice.dart';
+
 import 'package:sem_one_mobile_app_assignment/shared/Acategory.dart';
 
 class MyCategories extends StatelessWidget {
@@ -22,48 +22,43 @@ class MyCategories extends StatelessWidget {
                       "Bread",
                       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
                     ),
-                    navigateToPage: rice(),
+                    navigateToPage: "Breads",
                   ),
                   ACategory(
-                    CategoryItem: TheCategory(
-                      "Bread",
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
-                    ),
-                    navigateToPage: rice(),
-                  ),
+                      CategoryItem: TheCategory(
+                        "Bread",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
+                      ),
+                      navigateToPage: "Breads"),
                   ACategory(
-                    CategoryItem: TheCategory(
-                      "Salads",
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-                    ),
-                    navigateToPage: rice(),
-                  ),
+                      CategoryItem: TheCategory(
+                        "Salads",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                      ),
+                      navigateToPage: "Salads"),
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ACategory(
-                    CategoryItem: TheCategory(
-                      "Rice",
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
-                    ),
-                    navigateToPage: rice(),
-                  ),
+                      CategoryItem: TheCategory(
+                        "Rice",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
+                      ),
+                      navigateToPage: "Rice"),
                   ACategory(
-                    CategoryItem: TheCategory(
-                      "Rice",
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-                    ),
-                    navigateToPage: rice(),
-                  ),
+                      CategoryItem: TheCategory(
+                        "Rice",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                      ),
+                      navigateToPage: "Sweets"),
                   ACategory(
-                    CategoryItem: TheCategory(
-                      "Rice",
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-                    ),
-                    navigateToPage: rice(),
-                  ),
+                      CategoryItem: TheCategory(
+                        "Rice",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                      ),
+                      navigateToPage: "Appetizers"),
                 ],
               ),
             ],
@@ -76,30 +71,27 @@ class MyCategories extends StatelessWidget {
                 children: [
                   Flexible(
                     child: ACategory(
-                      CategoryItem: TheCategory(
-                        "Rice",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-                      ),
-                      navigateToPage: rice(),
-                    ),
+                        CategoryItem: TheCategory(
+                          "Rice",
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                        ),
+                        navigateToPage: "Rice"),
                   ),
                   Flexible(
                     child: ACategory(
-                      CategoryItem: TheCategory(
-                        "Bread",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
-                      ),
-                      navigateToPage: rice(),
-                    ),
+                        CategoryItem: TheCategory(
+                          "Bread",
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
+                        ),
+                        navigateToPage: "Rice"),
                   ),
                   Flexible(
                     child: ACategory(
-                      CategoryItem: TheCategory(
-                        "Salads",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-                      ),
-                      navigateToPage: rice(),
-                    ),
+                        CategoryItem: TheCategory(
+                          "Salads",
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                        ),
+                        navigateToPage: "Rice"),
                   ),
                 ],
               ),
@@ -108,21 +100,19 @@ class MyCategories extends StatelessWidget {
                 children: [
                   Flexible(
                     child: ACategory(
-                      CategoryItem: TheCategory(
-                        "Pasta",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
-                      ),
-                      navigateToPage: LoginPage(),
-                    ),
+                        CategoryItem: TheCategory(
+                          "Pasta",
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Lm7b6Kw7Z3tIxKZFFS0gV-jWDh9ozpORQ&s",
+                        ),
+                        navigateToPage: "Rice"),
                   ),
                   Flexible(
                     child: ACategory(
-                      CategoryItem: TheCategory(
-                        "Pizza",
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
-                      ),
-                      navigateToPage: rice(),
-                    ),
+                        CategoryItem: TheCategory(
+                          "Pizza",
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQUbOk2IklYhD0xLLk7PnCqKqZnkeEeIcYSQ&s",
+                        ),
+                        navigateToPage: "Rice"),
                   ),
                 ],
               ),
