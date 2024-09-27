ec2f51df16cd6aee31599f280167a799bef0ae38
FathimaShums <fathimashums416@gmail.com>
2024-09-27 10:37:59 +0530

    added some imports

--- lib/screens/ACategoryPage.dart
+++ lib/screens/ACategoryPage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:sem_one_mobile_app_assignment/main.dart';
 import 'package:sem_one_mobile_app_assignment/models/FoodItem.dart';
+import 'package:sem_one_mobile_app_assignment/screens/MyCart.dart';
+import 'package:sem_one_mobile_app_assignment/screens/MyCategories.dart';
+import 'package:sem_one_mobile_app_assignment/screens/MyFavourites.dart';
+import 'package:sem_one_mobile_app_assignment/screens/MyHomePage.dart';
+import 'package:sem_one_mobile_app_assignment/screens/MyProfile.dart';
 import 'package:sem_one_mobile_app_assignment/screens/ProductPageInfo.dart';
 import 'package:sem_one_mobile_app_assignment/shared/FoodItemCard.dart';
 
@@ -16,24 +21,12 @@ class Acategorypage extends StatefulWidget {
 }
 
 class _AcategorypageState extends State<Acategorypage> {
-  final List<FoodItem> items = [
-    FoodItem(
-        1,
-        "Maqluba",
-        "rice, you turn the pot upside down when serving",
-        "Includes fried vegetables such as eggplant, cauliflower, and potatoes, arranged in a pot before being cooked and inverted for presentation",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC7yHnSVgsZqy1WQEsiT-Dgjj1NjpxjrpZWA&s",
-        50.03,
-        "Rice"),
-    FoodItem(
-        2,
-        "Cream Cheese",
-        "Bread",
-        "",
-        "https://www.tashasartisanfoods.com/blog/wp-content/uploads/2023/04/Korean-Cream-Cheese-Garlic-Bread-8.jpeg",
-        50.03,
-        "Breads"),
-  ];
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   List<FoodItem> getFilteredItems() {
     return items.where((item) => item.ItsCategory == widget.oftype).toList();
@@ -71,20 +64,86 @@ class _AcategorypageState extends State<Acategorypage> {
     };
   }
 
+  // Method to show the categories
+  void _showCategories() {
+    showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(100.0, 100.0, 0.0, 0.0),
+      items: [
+        PopupMenuItem(
+          child: Text('Rice'),
+          value: 'Rice',
+        ),
+        PopupMenuItem(
+          child: Text('Bread'),
+          value: 'Bread',
+        ),
+        PopupMenuItem(
+          child: Text('Salads'),
+          value: 'Salads',
+        ),
+      ],
+      elevation: 8.0,
+    ).then((value) {
+      if (value != null) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Acategorypage(
+              orientation: widget.orientation,
+              oftype: value,
+            ),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     List<FoodItem> filteredItems = getFilteredItems();
+    List<Widget> _pages = <Widget>[
+      MyHomePage(orientation: widget.orientation),
+      MyCategories(orientation: widget.orientation),
+      MyFavourites(orientation: widget.orientation),
+      MyCart(orientation: widget.orientation),
+      MyProfile(orientation: widget.orientation),
+    ];
 
-    return ListView.builder(
-      itemCount: filteredItems.length,
-      itemBuilder: (context, index) {
-        return FoodItemCard(
-          TheFoodItem: filteredItems[index],
-          onPressed: AddToCart(filteredItems[index]),
-          whenPressed: AddToFavourites(filteredItems[index]),
-          onViewMore: NavigateToProductPage(filteredItems[index]),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.oftype),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: _showCategories,
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: filteredItems.length,
+        itemBuilder: (context, index) {
+          return FoodItemCard(
+            TheFoodItem: filteredItems[index],
+            onPressed: AddToCart(filteredItems[index]),
+            whenPressed: AddToFavourites(filteredItems[index]),
+            onViewMore: NavigateToProductPage(filteredItems[index]),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'Menu'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: 'Favourites'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_basket_outlined), label: 'Cart'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.supervised_user_circle), label: 'User'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
